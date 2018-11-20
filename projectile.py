e0d7a95c180a8fd3e54a33fd88c5c847fd62b75c
David Surry <djsurry@gmail.com>
2018-11-20 00:18:19 -0500

    added a bunch of new options

--- projectile.py
+++ projectile.py
@@ -1,7 +1,7 @@
 #ex physics
 import math, sys, cutie, readchar, os
 GRAVITY = -9.8
-
+SIZE = 10
 def quad(a, b, c):
 	a2 = a*2
 
@@ -16,7 +16,7 @@ def quad(a, b, c):
 
 def clear(n):
 	print("\033[{}A".format(n+1))
-	for i in range(n+1):
+	for i in range(n):
 		print("\033[K")
 	print("\033[{}A".format(n))
 
@@ -134,7 +134,7 @@ class Problem:
 		self.y.v2 = math.sqrt(2*self.y.a*self.y.s + self.y.v1*self.y.v1)
 
 	def range(self):
-		clear(7)
+		clear(SIZE)
 		print("Range is {0:.2f}m\n".format(self.x.s))
 
 	def maxHeight(self):
@@ -147,19 +147,68 @@ class Problem:
 		print("Max Height is {0:.2f}m\n".format(s))
 
 	def speedAtGround(self):
-		clear(7)
+		clear(SIZE)
 		print("Speed on impact with ground is {0:.2f}m/s\n".format(math.sqrt(self.x.v1 * self.x.v1 + self.y.v2 * self.y.v2)))
 
 	def velocityAtGround(self):
-		clear(7)
+		clear(SIZE)
 		print("Velocity on impact with ground is {0:.2f}m/s offset {1:.2f} degress down\n".format(math.sqrt(self.x.v1 * self.x.v1 + self.y.v2 * self.y.v2), math.degrees(math.atan(self.y.v2/self.x.v1))))
+	def totalTime(self):
+		clear(SIZE)
+		print("Time is {0:.2f}".format(self.x.t))
+	def xyT(self):
+		clear(SIZE)
+		toRemove = 0
+		while True:
+			t = input("At what time: ")
+			toRemove += 1
+			t = isFloat(t)
+			if t:
+				if t > self.x.t:
+					print("Out of domain")
+					toRemove += 1
+					continue
+				break
+		clear(toRemove)
+		# FOR X
+		# ds = None, dt = t, v1 = self.x.v1, v2 = self.x.v1, a = 0
+		x, y = splitVel(self.angle, self.initVel, self.height)
+		x.t = t
+		# ds = ((v1 + v2)/2*t
+		x.s = ((x.v1 + x.v2)/2)*x.t
+		# FOR Y
+		# ds = None, dt = t, a = -9.8, v1 = self.y.v1, v2 = None
+		y.t = t
+		# ds = v1 * t + 1/2 * a * t
+		y.s = y.v1 * y.t + 0.5*y.a*(y.t*y.t)
+		print("It has traveled {0:.2f}m and is {2:.2f}m off the ground at {1}s".format(x.s, x.t, self.height+y.s))
+
+	def xyD(self):
+		clear(SIZE)
+		print("Coming soon")
+		# toRemove = 2
+		# options = ["Find X from Y", "Find Y from X"]
+		# option = options[cutie.select(options, selected_index=0)]
+		# while True:
+		# 	t = input("At what distance? ")
+		# 	toRemove += 1
+		# 	t = isFloat(t)
+		# 	if t:
+		# 		if t > self.x.s and option =="Find Y from X":
+		# 			print("Out of domain")
+		# 			toRemove += 1
+		# 			continue
+
+
+
+
 
 def main():
 	i = getInputs()
 	p = Problem(isFloat(i[0]), isFloat(i[1]), isFloat(i[2]))
 	p.solve()
 	first = True
-	choices = ["What would you like to know?", "Range", "Maximum height", "Speed at impact on ground", "Velocity at impact on ground", "New problem", "Quit"]
+	choices = ["What would you like to know?", "Range", "Total Time", "X and Y at specific time", "X or Y at cooresponding distance", "Maximum height", "Speed at impact on ground", "Velocity at impact on ground", "New problem", "Quit"]
 	while True:
 		choice = choices[cutie.select(choices, caption_indices=[0], selected_index=1)]
 		if choice == "Range":
@@ -168,6 +217,12 @@ def main():
 			sys.exit(0)
 		elif choice == "Maximum height":
 			p.maxHeight()
+		elif choice == "X and Y at specific time":
+			p.xyT()
+		elif choice == "X or Y at cooresponding distance":
+			p.xyD()
+		elif choice == "Total Time":
+			p.totalTime()
 		elif choice == "Speed at impact on ground":
 			p.speedAtGround()
 		elif choice == "Velocity at impact on ground":
