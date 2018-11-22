2a99116dc7662c737b001e57cae7f43689fe80f9
David Surry <djsurry@gmail.com>
2018-11-21 21:06:43 -0500

    fixed install

--- setup.c
+++ setup.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
+#include <errno.h>
 
 int main(){
+	DIR* dir = opendir("/usr/local/bin");
+        if (dir)
+	{
+	system("mv ./projectile /usr/local/bin");
+    	closedir(dir);
+	}
+	else if (ENOENT == errno)
+	{
+		system("mv ./projectile /usr/bin");
+	}
 	system("mv ./projectile /usr/local/bin");
 	printf("All ready to go! Just open a terminal and write \"projectile\" to start the program!\n");
 	return 0;
