ed3bb061ab8bd0461567a8b8047818eb10e42594
David Surry <djsurry@gmail.com>
2018-11-21 21:14:42 -0500

    finally finished install

--- setup.c
+++ setup.c
@@ -8,14 +8,13 @@ int main(){
 	DIR* dir = opendir("/usr/local/bin");
         if (dir)
 	{
-	system("mv ./projectile /usr/local/bin");
+	system("cp ./projectile /usr/local/bin");
     	closedir(dir);
 	}
 	else if (ENOENT == errno)
 	{
-		system("mv ./projectile /usr/bin");
+		system("cp ./projectile /usr/bin");
 	}
-	system("mv ./projectile /usr/local/bin");
 	printf("All ready to go! Just open a terminal and write \"projectile\" to start the program!\n");
 	return 0;
 }
