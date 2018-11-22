96bd2a0600d0a44c1539dc3caa787191d5212d45
David Surry <djsurry@gmail.com>
2018-11-22 08:51:46 -0500

    fixed

--- setup.c
+++ setup.c
@@ -5,8 +5,9 @@
 #include <errno.h>
 
 int main(){
+        system("mkdir /usr/local/bin");
+        system("echo '/usr/local/bin' >> /etc/paths");
 	system("cp ./projectile /usr/local/bin");
-		system("cp ./projectile /usr/bin");
 	printf("All ready to go! Just open a terminal and write \"projectile\" to start the program!\n");
 	return 0;
 }
